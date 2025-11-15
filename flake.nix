6402ffecfcd3d7445faf0410de6b660feb938480
Akira Komamura <akira.komamura@gmail.com>
2025-11-16 02:37:32 +0900

    Add Nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs
+            pkgs.corepack
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
